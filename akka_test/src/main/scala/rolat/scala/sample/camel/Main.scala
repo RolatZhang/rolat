5c620ca883505512bed776d32b9a2bf0336823e1
feiyue5216 <896997820@qq.com>
2018-08-02 17:51:36 +0800

    测试代码提交

--- akka_test/src/main/scala/rolat/scala/sample/camel/Main.scala
+++ akka_test/src/main/scala/rolat/scala/sample/camel/Main.scala
@@ -1,9 +1,25 @@
 package rolat.scala.sample.camel
 
+import akka.Main.Terminator
+import akka.actor.{Actor, ActorSystem, ExtendedActorSystem, Props}
+
+import scala.util.control.NonFatal
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    akka.Main.main(Array(classOf[HelloWorld].getName))
+    println(classOf[HelloWorld].getName)
+   /* akka.Main.main(Array(classOf[HelloWorld].getName))*/
+
+    val system = ActorSystem("Main")
+    try {
+      val appClass = system.asInstanceOf[ExtendedActorSystem].dynamicAccess.getClassFor[Actor](classOf[HelloWorld].getName).get
+      println(appClass)
+      val app = system.actorOf(Props(appClass), "app")
+      val terminator = system.actorOf(Props(classOf[Terminator], app), "app-terminator")
+    } catch {
+      case NonFatal(e) ⇒ system.terminate(); throw e
+    }
   }
 
 }
